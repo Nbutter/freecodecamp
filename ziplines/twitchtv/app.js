8fc25b44ba79641db27705473e772d595a1a048b
Nicholas Butterworth <nicholas.butterworth@gmail.com>
2015-06-08 14:40:12 -0400

    twitch api working

--- ziplines/twitchtv/app.js
+++ ziplines/twitchtv/app.js
@@ -1,5 +1,23 @@
 console.log("app.js loaded");
 
+var nameArray = ["freecodecamp", "storbeck", "MedryBW", "terakilobyte", "habathcx", "RobotCaleb", "comster404", "brunofin", "thomasballinger", "noobs2ninjas", "beohoff"];
+
 $(function(){
-	$('#main').html("JQUERY WORKING...");
+	for (var i = 0; i < nameArray.length; i++){
+		makeStatusDisplay(nameArray[i]);
+	}	
 });
+
+var makeStatusDisplay = function(channelName){
+	var htmlString = '<div class="status-display" id=' + channelName 
+	+ '><a href="http://www.twitch.tv/' + channelName + '"><span class="channel-name">' + channelName 
+	+ '</span><i class="fa fa-exclamation"></i></a></div>';
+	var display = $('#status').append(htmlString);
+	var url = 'https://api.twitch.tv/kraken/streams/' + channelName + '?callback=?&api_version=2';
+	$.getJSON(url, function(response){
+		if(response.stream){
+			$('#' + channelName + " i").removeClass('fa-exclamation').addClass('fa-check');
+			$('#' + channelName).append("<p>Now playing: " + response.stream.game + "</p>");
+		} 
+	})
+}
